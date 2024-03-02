c21dd9ca40079bc08d2a01564ab70b6e56eae0d7
LVQT <levietquocthinh@gmail.com>
2024-03-02 13:45:20 +0700

    method GET for Realestate

--- src/main/java/tech/rent/be/controller/RealEstateController.java
+++ src/main/java/tech/rent/be/controller/RealEstateController.java
@@ -3,13 +3,13 @@ package tech.rent.be.controller;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import tech.rent.be.dto.PostResponseDTO;
 import tech.rent.be.dto.RealEstateDTO;
 import tech.rent.be.entity.RealEstate;
 import tech.rent.be.services.RealEstateService;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
@@ -23,5 +23,13 @@ public class RealEstateController {
         RealEstate realEstate = realEstateService.createEstate(realEstateDTO);
         return ResponseEntity.ok(realEstate);
     }
+    @GetMapping("/showEstate")
+    public  ResponseEntity<List<RealEstateDTO>> getAllRealEstate(){
+       List<RealEstateDTO> estates = realEstateService.getAllRealEstate();
+        return ResponseEntity.ok(estates);
+    }
+
+
+
 
 }
--- src/main/java/tech/rent/be/services/RealEstateService.java
+++ src/main/java/tech/rent/be/services/RealEstateService.java
@@ -3,6 +3,7 @@ package tech.rent.be.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import tech.rent.be.dto.PostResponseDTO;
 import tech.rent.be.dto.RealEstateDTO;
 import tech.rent.be.dto.ResourceDTO;
 import tech.rent.be.entity.*;
@@ -33,56 +34,72 @@ public class RealEstateService {
 
     @Autowired
     LocationRepository locationRepository;
-        public RealEstate createEstate(RealEstateDTO realEstateDTO){
-            Users users = accountUtils.getCurrentUser();
-            Category category = categoryRepository.findCategoryById(realEstateDTO.getCategoryId());
-            Location location = locationRepository.findLocationById(realEstateDTO.getLocationId());
-            RealEstate realEstate = new RealEstate();
-            realEstate.setDescription(realEstateDTO.getDescription());
-            realEstate.setTitle(realEstateDTO.getTitle());
-            realEstate.setDescription(realEstateDTO.getDescription());
-            realEstate.setDate(realEstateDTO.getDate());
-            realEstate.setAmount(realEstateDTO.getAmount());
-            realEstate.setCategory(category);
-            realEstate.setLocation(location);
-            realEstate.setUsers(users);
 
-            List<RealEstate> realEstates = category.getEstates();
-
-            if(realEstates == null){
-                realEstates = new ArrayList<>();
-            }
-
-            realEstates.add(realEstate);
-
-            category.setEstates(realEstates);
-
-
-            List<RealEstate> realEstate02 = category.getEstates();
-
-            if(realEstate02 == null){
-                realEstate02 = new ArrayList<>();
-            }
-
-            realEstate02.add(realEstate);
-
-            category.setEstates(realEstate02);
-
-
-
-
-
-            List<Resource> resources = new ArrayList<>();
-            // ResourceDTO => Resource
-            for(ResourceDTO resourceDTO: realEstateDTO.getResources()){
-                Resource resource = new Resource();
-                resource.setResourceType(resourceDTO.getResourceType());
-                resource.setUrl(resourceDTO.getUrl());
-                resource.setRealEstate(realEstate);
-                resources.add(resource);
-            }
-            realEstate.setResource(resources);
-
-            return realEstateRepository.save(realEstate);
+    public RealEstate createEstate(RealEstateDTO realEstateDTO) {
+        Users users = accountUtils.getCurrentUser();
+        Category category = categoryRepository.findCategoryById(realEstateDTO.getCategoryId());
+        Location location = locationRepository.findLocationById(realEstateDTO.getLocationId());
+        RealEstate realEstate = new RealEstate();
+        realEstate.setDescription(realEstateDTO.getDescription());
+        realEstate.setTitle(realEstateDTO.getTitle());
+        realEstate.setDescription(realEstateDTO.getDescription());
+        realEstate.setDate(realEstateDTO.getDate());
+        realEstate.setAmount(realEstateDTO.getAmount());
+        realEstate.setCategory(category);
+        realEstate.setLocation(location);
+        realEstate.setUsers(users);
+        List<RealEstate> realEstates = category.getEstates();
+        if (realEstates == null) {
+            realEstates = new ArrayList<>();
         }
+        realEstates.add(realEstate);
+        category.setEstates(realEstates);
+        List<RealEstate> realEstate02 = category.getEstates();
+        if (realEstate02 == null) {
+            realEstate02 = new ArrayList<>();
+        }
+        realEstate02.add(realEstate);
+        category.setEstates(realEstate02);
+        List<Resource> resources = new ArrayList<>();
+        // ResourceDTO => Resource
+        for (ResourceDTO resourceDTO : realEstateDTO.getResources()) {
+            Resource resource = new Resource();
+            resource.setResourceType(resourceDTO.getResourceType());
+            resource.setUrl(resourceDTO.getUrl());
+            resource.setRealEstate(realEstate);
+            resources.add(resource);
+        }
+        realEstate.setResource(resources);
+
+        return realEstateRepository.save(realEstate);
+    }
+
+
+    public List<RealEstateDTO> getAllRealEstate() {
+        List<RealEstate> estateList = realEstateRepository.findAll();
+        List<RealEstateDTO> estateDTOList = new ArrayList<>();
+        for (RealEstate realEstate : estateList) {
+            RealEstateDTO realEstateDTO = new RealEstateDTO();
+            //Map
+            realEstateDTO.setId(realEstate.getId());
+            realEstateDTO.setTitle(realEstate.getTitle());
+            realEstateDTO.setDescription(realEstate.getDescription());
+            realEstateDTO.setDate(realEstate.getDate());
+            realEstateDTO.setAmount(realEstate.getAmount());
+            estateDTOList.add(realEstateDTO);
+
+            List<ResourceDTO> resourceDTOS = new ArrayList<>();
+            // ResourceDTO => Resource
+            for (Resource resource : realEstate.getResource()) {
+                ResourceDTO resourceDTO = new ResourceDTO();
+                resourceDTO.setResourceType(resource.getResourceType());
+                resourceDTO.setUrl(resource.getUrl());
+                resourceDTOS.add(resourceDTO);
+            }
+            realEstateDTO.setResources(resourceDTOS);
+
+        }
+        return estateDTOList;
+    }
+
 }
